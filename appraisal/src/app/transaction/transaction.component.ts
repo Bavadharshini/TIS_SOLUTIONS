d7b6566835b017efeec8b2a683165fb39b572ec1
Bavadharshini Kandhan <bavadharshini.kandhan@ibm.com>
2024-05-29 09:30:54 +0530

    Routing component

--- appraisal/src/app/transaction/transaction.component.ts
+++ appraisal/src/app/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Transaction } from '../Transaction';
 
 @Component({
   selector: 'app-transaction',
@@ -8,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './transaction.component.css'
 })
 export class TransactionComponent {
+  fullStackExpenseQ1: Transaction[] = [];
+  fullStackExpenseQ2: Transaction[] = [];
+  
+  fullStackExpenseQ3: Transaction[] = [];
+  fullStackExpenseQ4: Transaction[] = [];
 
+  SAPExpenseQ1: Transaction[] = [];
+  SAPExpenseQ2: Transaction[] = [];
+  SAPExpenseQ3: Transaction[] = [];
+  SAPExpenseQ4: Transaction[] = [];
+
+  SalesforceExpenseQ1: Transaction[] = [];
+  SalesforceExpenseQ2: Transaction[] = [];
+  SalesforceExpenseQ3: Transaction[] = [];
+  SalesforceExpenseQ4: Transaction[] = [];
+
+  financeExpenseQ1: Transaction[] = [];
+  financeExpenseQ2: Transaction[] = [];
+  financeExpenseQ3: Transaction[] = [];
+  financeExpenseQ4: Transaction[] = [];
+
+
+
+
+
+
+
+
+ 
 }
